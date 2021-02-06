762eee068a0296064b54bccf2bb4b697c7a9136b
Ilnur Saifutdinov <ilnursoft@gmail.com>
2021-02-06 23:37:30 +0300

    добавление учебного проекта BeatBox из книги Изучаем Java

---
+++ beatbox/src/MusicTest1.java
@@ -0,0 +1,18 @@
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+
+    public void play() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer(); // синтезатор
+            System.out.println("Успешно получили синтезатор");
+        } catch (MidiUnavailableException ex) {
+            System.out.println("Неудача");
+        }
+    }
+
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+}
