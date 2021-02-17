794cd3c3241489020d6f425d9cc46dc64b67508a
Ilnur Saifutdinov <ilnursoft@gmail.com>
2021-02-17 22:22:07 +0300

    add gui miniplayer 2 and 3

---
+++ beatbox/src/MiniMusicPlayer2.java
@@ -0,0 +1,55 @@
+import javax.sound.midi.*;
+
+// регистрируем и получаем ControllerEvent
+
+// реализуем интерфейс слушателя
+public class MiniMusicPlayer2 implements ControllerEventListener {
+
+    public static void main(String[] args) {
+        MiniMusicPlayer2 mini = new MiniMusicPlayer2();
+        mini.go();
+    }
+
+    public void go() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            // регистрируем события синтезатором
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(this, eventsIWant);
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            for (int i = 5; i < 61; i += 4) {
+                track.add(makeEvent(144, 1, i, 100, i));
+
+                track.add(makeEvent(176, 1, 127, 0, i));
+
+                track.add(makeEvent(128, 1, i, 100, i + 2));
+            }
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void controlChange(ShortMessage event) {
+        System.out.println("ля");
+    }
+
+
+    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+        }
+        return event;
+    }
+}
